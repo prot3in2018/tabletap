97bd788298ace74bd99acee7f8f36675d24d6144
加藤拓人 <takutokato@katoutakutonoMacBook-Pro.local>
2025-05-15 13:26:47 +1000

    Force superuser creation with staff rights

--- core/management/commands/createadmin.py
+++ core/management/commands/createadmin.py
@@ -2,15 +2,25 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
 class Command(BaseCommand):
-    help = 'Creates the default admin user'
+    help = 'Force create superuser'
 
     def handle(self, *args, **kwargs):
         username = 's4661478'
         email = 'takuto.kato@student.uq.edu.au'
         password = 'infs3202'
 
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username=username, email=email, password=password)
-            self.stdout.write(self.style.SUCCESS(f'Successfully created superuser: {username}'))
-        else:
-            self.stdout.write(self.style.WARNING(f'Superuser "{username}" already exists'))
+        try:
+            user = User.objects.get(username=username)
+            user.set_password(password)
+            user.is_staff = True
+            user.is_superuser = True
+            user.email = email
+            user.save()
+            self.stdout.write(self.style.SUCCESS(f'Updated superuser: {username}'))
+        except User.DoesNotExist:
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
+            self.stdout.write(self.style.SUCCESS(f'Created new superuser: {username}'))
